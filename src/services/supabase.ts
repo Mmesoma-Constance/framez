1b345294718890d16ca8e8546ee9ff921125083a
Mmesoma-Constance <mmesomanwokem@gmail.com>
2025-11-14 11:22:04 +0100

    Moved supabase keys to env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const supabaseUrl = 'https://nxheocovjzhsmrpaxeql.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54aGVvY292anpoc21ycGF4ZXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MTcxNzQsImV4cCI6MjA3ODI5MzE3NH0.yoL7clkIGXRjQXIYzY1kisX_k3CGUqd2UMsLQ2cgsOI';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
